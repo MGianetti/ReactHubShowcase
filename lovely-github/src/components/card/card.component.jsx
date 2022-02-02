8f1c8550e1699c433043e962832d0be7580d0a81
MGianetti <mateusgianetti1996@gmail.com>
2022-02-02 16:47:15 -0300

    Implemented Card initial setup

---
+++ lovely-github/src/components/card/card.component.jsx
@@ -0,0 +1,7 @@
+export const dataTestIds = {};
+
+function Card() {
+  return <div>Card</div>;
+}
+
+export default Card;
---
+++ lovely-github/src/components/card/card.test.jsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import Card, { dataTestIds } from "./card.component";
+
+describe("Describe <Card/>", () => {
+  const defaultProps = {};
+  const setUpComponent = (props) => {
+    const componentProps = { ...defaultProps, ...props };
+    return render(<Card {...componentProps} />);
+  };
+  it("Should be defined", () => {
+    const view = setUpComponent();
+    expect(view).toBeDefined();
+  });
+});
