9c7ebb5394945a0708950705cf73d4cc637ae300
MGianetti <mateusgianetti1996@gmail.com>
2022-02-02 16:46:25 -0300

    Implemented Input initial setup

---
+++ lovely-github/src/components/input/input.component.jsx
@@ -0,0 +1,7 @@
+export const dataTestIds = {};
+
+function Input() {
+  return <div>Input</div>;
+}
+
+export default Input;
---
+++ lovely-github/src/components/input/input.test.jsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import Input, { dataTestIds } from "./input.component";
+
+describe("Describe <Input/>", () => {
+  const defaultProps = {};
+  const setUpComponent = (props) => {
+    const componentProps = { ...defaultProps, ...props };
+    return render(<Input {...componentProps} />);
+  };
+  it("Should be defined", () => {
+    const view = setUpComponent();
+    expect(view).toBeDefined();
+  });
+});
