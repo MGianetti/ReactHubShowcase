efe1bfcf5730711d260bd58e6a8ed5e81c7ba820
MGianetti <mateusgianetti1996@gmail.com>
2022-02-02 19:21:48 -0300

    Implemented SearchButton testing

--- lovely-github/src/components/search-button/search-button.test.jsx
+++ lovely-github/src/components/search-button/search-button.test.jsx
@@ -1,5 +1,4 @@
-import { render } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
 
 import SearchButton, { dataTestIds } from "./search-button.component";
 
@@ -14,5 +13,26 @@ describe("Describe <SearchButton/>", () => {
   it("Should be defined", () => {
     const view = setUpComponent();
     expect(view).toBeDefined();
+  });
+
+  it("Should render button correctly", () => {
+    setUpComponent();
+    const searchButton = screen.getByTestId(dataTestIds.searchButton);
+    const searchIcon = screen.getByTestId(dataTestIds.searchIcon);
+    expect(searchButton).toBeInTheDocument();
+    expect(searchIcon).toBeInTheDocument();
+  });
+
+  it("Should render enabled as default", () => {
+    setUpComponent();
+    const searchButton = screen.getByTestId(dataTestIds.searchButton);
+    expect(searchButton).toBeEnabled();
+  });
+
+  it("Should render disabled if prop disabled is true", () => {
+    const disabled = true;
+    setUpComponent({ disabled });
+    const searchButton = screen.getByTestId(dataTestIds.searchButton);
+    expect(searchButton).toBeDisabled();
   });
 });
