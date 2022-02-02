8fb50719491580a57b407f880b085d4baf710115
MGianetti <mateusgianetti1996@gmail.com>
2022-02-02 18:59:11 -0300

    Implemented TextButton requirements

--- lovely-github/src/components/text-button/text-button.component.jsx
+++ lovely-github/src/components/text-button/text-button.component.jsx
@@ -1,7 +1,45 @@
-export const dataTestIds = {};
+import PropTypes from "prop-types";
+import classNames from "classnames";
 
-function TextButton() {
-  return <div>TextButton</div>;
+import styles from "./text-button.module.scss";
+
+export const dataTestIds = {
+  textButtonId: "data-testid-text-button",
+};
+
+function TextButton(props) {
+  const { type, className, text, onClick, disabled } = props;
+
+  const textButtonClasses = classNames(styles.TextButton, className);
+
+  return (
+    <button
+      type={type}
+      text={text}
+      onClick={onClick}
+      className={textButtonClasses}
+      disabled={disabled}
+      data-testid={dataTestIds.textButtonId}
+    >
+      {text}
+    </button>
+  );
 }
+
+TextButton.propTypes = {
+  type: PropTypes.string,
+  text: PropTypes.string,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+};
+
+TextButton.defaultProps = {
+  className: "",
+  type: "button",
+  text: "TextButton",
+  disabled: true,
+  onClick: () => undefined,
+};
 
 export default TextButton;
