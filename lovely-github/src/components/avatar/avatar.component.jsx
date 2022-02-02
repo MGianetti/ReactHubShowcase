c7f86abe320e5b2a94bb290d369604eac251103c
MGianetti <mateusgianetti1996@gmail.com>
2022-02-02 16:49:55 -0300

    Implemented Avatar initial setup

---
+++ lovely-github/src/components/avatar/avatar.component.jsx
@@ -0,0 +1,7 @@
+export const dataTestIds = {};
+
+function Avatar() {
+  return <div>Avatar</div>;
+}
+
+export default Avatar;
---
+++ lovely-github/src/components/avatar/avatar.test.jsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import Avatar, { dataTestIds } from "./avatar.component";
+
+describe("Describe <Avatar/>", () => {
+  const defaultProps = {};
+  const setUpComponent = (props) => {
+    const componentProps = { ...defaultProps, ...props };
+    return render(<Avatar {...componentProps} />);
+  };
+  it("Should be defined", () => {
+    const view = setUpComponent();
+    expect(view).toBeDefined();
+  });
+});
