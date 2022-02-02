7636dc08a8cb65635cbf12c4345874723664b1b2
MGianetti <mateusgianetti1996@gmail.com>
2022-02-02 16:37:30 -0300

    Implemented TextButton initial setup

---
+++ lovely-github/src/components/text-button/text-button.component.jsx
@@ -0,0 +1,7 @@
+export const dataTestIds = {};
+
+function TextButton() {
+  return <div>TextButton</div>;
+}
+
+export default TextButton;
---
+++ lovely-github/src/components/text-button/text-button.test.jsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import TextButton, { dataTestIds } from "./text-button.component";
+
+describe("Describe <TextButton/>", () => {
+  const defaultProps = {};
+  const setUpComponent = (props) => {
+    const componentProps = { ...defaultProps, ...props };
+    return render(<TextButton {...componentProps} />);
+  };
+  it("Should be defined", () => {
+    const view = setUpComponent();
+    expect(view).toBeDefined();
+  });
+});
