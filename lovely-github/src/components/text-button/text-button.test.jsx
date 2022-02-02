c5ce3282191c004455f37da0bc8782ef8d58e3b6
MGianetti <mateusgianetti1996@gmail.com>
2022-02-02 18:56:05 -0300

    Implemented TextButton testing

--- lovely-github/src/components/text-button/text-button.test.jsx
+++ lovely-github/src/components/text-button/text-button.test.jsx
@@ -1,18 +1,35 @@
-import { render } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import React from "react";
+import { render, screen } from "@testing-library/react";
 
 import TextButton, { dataTestIds } from "./text-button.component";
 
 describe("Describe <TextButton/>", () => {
-  const defaultProps = {};
-
   const setUpComponent = (props) => {
-    const componentProps = { ...defaultProps, ...props };
-    return render(<TextButton {...componentProps} />);
+    return render(<TextButton {...props} />);
   };
 
   it("Should be defined", () => {
     const view = setUpComponent();
     expect(view).toBeDefined();
+  });
+
+  it("Should render correct text when passed via props", () => {
+    const text = "Awesome text";
+    setUpComponent({ text });
+    const customTextButton = screen.getByText(text);
+    expect(customTextButton).toBeInTheDocument();
+  });
+
+  it("Should render disabled as default", () => {
+    setUpComponent();
+    const textButton = screen.getByTestId(dataTestIds.textButtonId);
+    expect(textButton).toBeDisabled();
+  });
+
+  it("Should render enabled if prop enabled is true", () => {
+    const disabled = false;
+    setUpComponent({ disabled });
+    const textButton = screen.getByTestId(dataTestIds.textButtonId);
+    expect(textButton).toBeEnabled();
   });
 });
