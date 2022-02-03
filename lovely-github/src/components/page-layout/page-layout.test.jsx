8357174ee054529c9e5d2997bae1a4310c6bae83
MGianetti <mateusgianetti1996@gmail.com>
2022-02-03 12:35:44 -0300

    Implemented PageLayout testing

--- lovely-github/src/components/page-layout/page-layout.test.jsx
+++ lovely-github/src/components/page-layout/page-layout.test.jsx
@@ -1,9 +1,8 @@
-import { render } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
 
 import PageLayout, { dataTestIds } from "./page-layout.component";
 
-describe("Describe <PageLayout/>", () => {
+describe("Describe <PageLayout/>:", () => {
   const defaultProps = {};
 
   const setUpComponent = (props) => {
@@ -14,5 +13,16 @@ describe("Describe <PageLayout/>", () => {
   it("Should be defined", () => {
     const view = setUpComponent();
     expect(view).toBeDefined();
+  });
+
+  it("Should encapsulate children node elements", () => {
+    const spanLabel = "Hello world";
+    const children = <span>{spanLabel}</span>;
+
+    setUpComponent({ children });
+    const pageLayout = screen.getByTestId(dataTestIds.layout);
+    const innerChildren = screen.getByText(spanLabel);
+    expect(pageLayout).toBeDefined();
+    expect(innerChildren).toBeDefined();
   });
 });
