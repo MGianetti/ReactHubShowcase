91285c3c8c2a41051d4653539504f4859c43a718
MGianetti <mateusgianetti1996@gmail.com>
2022-02-03 11:34:39 -0300

    Implemented Input requirements

--- lovely-github/src/components/input/input.component.jsx
+++ lovely-github/src/components/input/input.component.jsx
@@ -1,7 +1,37 @@
-export const dataTestIds = {};
+import PropTypes from "prop-types";
+import classNames from "classnames";
 
-function Input() {
-  return <div>Input</div>;
+import styles from "./input.module.scss";
+
+export const dataTestIds = { input: "data-testid-input" };
+
+function Input(props) {
+  const { className, onChange, value, placeholder } = props;
+
+  const inputClasses = classNames(styles.Input, className);
+
+  return (
+    <input
+      value={value}
+      className={inputClasses}
+      data-testid={dataTestIds.input}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
+  );
 }
+
+Input.propTypes = {
+  className: PropTypes.string,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func,
+  value: PropTypes.string.isRequired,
+};
+
+Input.defaultProps = {
+  className: "",
+  placeholder: "",
+  onChange: () => undefined,
+};
 
 export default Input;
