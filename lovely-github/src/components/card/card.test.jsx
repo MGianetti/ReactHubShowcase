e3a6ec7423f1b648dd63d980a304ef172562af9e
MGianetti <mateusgianetti1996@gmail.com>
2022-02-03 16:32:44 -0300

    Implemented Card testing

--- lovely-github/src/components/card/card.test.jsx
+++ lovely-github/src/components/card/card.test.jsx
@@ -1,10 +1,9 @@
-import { render } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
 
 import Card, { dataTestIds } from "./card.component";
 
-describe("Describe <Card/>", () => {
-  const defaultProps = {};
+describe("Component <Card/>:", () => {
+  const defaultProps = { width: 200, minHeight: 300 };
 
   const setUpComponent = (props) => {
     const componentProps = { ...defaultProps, ...props };
@@ -14,5 +13,16 @@ describe("Describe <Card/>", () => {
   it("Should be defined", () => {
     const view = setUpComponent();
     expect(view).toBeDefined();
+  });
+
+  it("Should encapsulate children node elements", () => {
+    const spanLabel = "Hello world";
+    const children = <span>{spanLabel}</span>;
+
+    setUpComponent({ children });
+    const card = screen.getByTestId(dataTestIds.card);
+    const innerChildren = screen.getByText(spanLabel);
+    expect(card).toBeDefined();
+    expect(innerChildren).toBeDefined();
   });
 });
