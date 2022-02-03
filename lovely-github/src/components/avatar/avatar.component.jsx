d186e318ddbd818e9f7f50f6ab76877cfcd1427c
MGianetti <mateusgianetti1996@gmail.com>
2022-02-03 11:54:55 -0300

    Implemented Avatar requirements

--- lovely-github/src/components/avatar/avatar.component.jsx
+++ lovely-github/src/components/avatar/avatar.component.jsx
@@ -1,7 +1,35 @@
-export const dataTestIds = {};
+import PropTypes from "prop-types";
+import classNames from "classnames";
 
-function Avatar() {
-  return <div>Avatar</div>;
+import styles from "./avatar.module.scss";
+
+export const dataTestIds = { avatar: "data-testid-avatar" };
+
+function Avatar(props) {
+  const { className, src, alt } = props;
+
+  const avatarClasses = classNames(styles.Avatar, className);
+
+  return (
+    <img
+      className={avatarClasses}
+      src={src}
+      alt={alt}
+      data-testid={dataTestIds.avatar}
+    />
+  );
 }
+
+Avatar.propTypes = {
+  className: PropTypes.string,
+  src: PropTypes.string,
+  alt: PropTypes.string,
+};
+
+Avatar.defaultProps = {
+  className: "",
+  src: "/default_user.png",
+  alt: "",
+};
 
 export default Avatar;
