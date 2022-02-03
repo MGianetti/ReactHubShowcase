f91c34052b428b1aa719b6aa994da0a289e45fb4
MGianetti <mateusgianetti1996@gmail.com>
2022-02-03 18:05:29 -0300

    Removed unnecessary import from Typography testing file

--- lovely-github/src/components/typography/typography.test.jsx
+++ lovely-github/src/components/typography/typography.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 
-import Typography, { dataTestIds } from "./typography.component";
+import Typography from "./typography.component";
 
 describe("Component <Typography/>", () => {
   const defaultProps = { type: "h1" };
