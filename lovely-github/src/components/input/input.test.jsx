54c159bc9789b499c61e9421b0baeb9b6d5f26fd
MGianetti <mateusgianetti1996@gmail.com>
2022-02-03 11:34:23 -0300

    Implemented Input testing

--- lovely-github/src/components/input/input.test.jsx
+++ lovely-github/src/components/input/input.test.jsx
@@ -1,10 +1,9 @@
-import { render } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
 
 import Input, { dataTestIds } from "./input.component";
 
 describe("Describe <Input/>", () => {
-  const defaultProps = {};
+  const defaultProps = { value: "" };
 
   const setUpComponent = (props) => {
     const componentProps = { ...defaultProps, ...props };
@@ -14,5 +13,21 @@ describe("Describe <Input/>", () => {
   it("Should be defined", () => {
     const view = setUpComponent();
     expect(view).toBeDefined();
+  });
+
+  it("Should render dynamic placeholder", () => {
+    const placeholder = "Type username";
+    setUpComponent({ placeholder });
+    const input = screen.getByTestId(dataTestIds.input);
+    expect(input).toBeDefined();
+    expect(input).toHaveAttribute("placeholder", placeholder);
+  });
+
+  it("Should have a controlled value prop", () => {
+    const value = "value";
+    setUpComponent({ value });
+    const input = screen.getByTestId(dataTestIds.input);
+    expect(input).toBeDefined();
+    expect(input).toHaveAttribute("value", value);
   });
 });
