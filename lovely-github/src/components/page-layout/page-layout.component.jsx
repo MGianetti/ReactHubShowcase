bc4a8df022f89736da16321dc017c83c2f78e202
MGianetti <mateusgianetti1996@gmail.com>
2022-02-03 12:35:58 -0300

    Implemented PageLayout requirements

--- lovely-github/src/components/page-layout/page-layout.component.jsx
+++ lovely-github/src/components/page-layout/page-layout.component.jsx
@@ -1,7 +1,31 @@
-export const dataTestIds = {};
+import { Fragment } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
 
-function PageLayout() {
-  return <div>PageLayout</div>;
+import styles from "./page-layout.module.scss";
+
+export const dataTestIds = { layout: "data-testid-page-layout" };
+
+function PageLayout(props) {
+  const { children, className } = props;
+
+  const layoutClasses = classNames(styles.Layout, className);
+
+  return (
+    <div className={layoutClasses} data-testid={dataTestIds.layout}>
+      {children}
+    </div>
+  );
 }
+
+PageLayout.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+PageLayout.defaultProps = {
+  className: "",
+  children: <Fragment></Fragment>,
+};
 
 export default PageLayout;
