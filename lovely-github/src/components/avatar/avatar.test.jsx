681ea9cd4ac8a21759b30da4d75c33a69362e0cc
MGianetti <mateusgianetti1996@gmail.com>
2022-02-03 11:54:28 -0300

    Implemented Avatar testing

--- lovely-github/src/components/avatar/avatar.test.jsx
+++ lovely-github/src/components/avatar/avatar.test.jsx
@@ -1,5 +1,4 @@
-import { render } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
 
 import Avatar, { dataTestIds } from "./avatar.component";
 
@@ -14,5 +13,21 @@ describe("Describe <Avatar/>", () => {
   it("Should be defined", () => {
     const view = setUpComponent();
     expect(view).toBeDefined();
+  });
+
+  it("Should allow dynamic alt description", () => {
+    const alt = "User Avatar";
+    setUpComponent({ alt });
+    const avatar = screen.getByTestId(dataTestIds.avatar);
+    expect(avatar).toBeDefined();
+    expect(avatar).toHaveAttribute("alt", alt);
+  });
+
+  it("Should allow dynamic image source", () => {
+    const src = "https://avatars.githubusercontent.com/u/39316813?v=4";
+    setUpComponent({ src });
+    const avatar = screen.getByTestId(dataTestIds.avatar);
+    expect(avatar).toBeDefined();
+    expect(avatar).toHaveAttribute("src", src);
   });
 });
