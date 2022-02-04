cccea5305f4e6208fd06918cbd3a218eab1c663f
MGianetti <mateusgianetti1996@gmail.com>
2022-02-04 13:19:25 -0300

    Implemented HomePage and UserPage rendering testing

--- lovely-github/src/pages/home-page/home-page.test.jsx
+++ lovely-github/src/pages/home-page/home-page.test.jsx
@@ -1,6 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 
+import { dataTestIds as cardDataTestid } from "../../components/card/card.component";
+import { dataTestIds as avatarDataTestid } from "../../components/avatar/avatar.component";
+import { dataTestIds as typographyDataTestid } from "../../components/typography/typography.component";
+import { dataTestIds as textButtonTestid } from "../../components/text-button/text-button.component";
+import { dataTestIds as searchButtonDataTestid } from "../../components/search-button/search-button.component";
 import HomePage from "./home-page.component";
 
 describe("Component <HomePage/>", () => {
@@ -19,8 +24,22 @@ describe("Component <HomePage/>", () => {
     const view = setUpComponent();
     expect(view).toBeDefined();
   });
-  //   it("Should correctly render screen", () => {
-  //     const view = setUpComponent();
-  //     expect(view).toBeDefined();
-  //   });
+
+  it("Should correctly render screen", () => {
+    setUpComponent();
+    const cards = screen.queryAllByTestId(cardDataTestid.card);
+    const searchButton = screen.queryAllByTestId(
+      searchButtonDataTestid.searchButton
+    );
+    const avatar = screen.queryAllByTestId(avatarDataTestid.avatar);
+    const typographies = screen.queryAllByTestId(
+      typographyDataTestid.typography
+    );
+    const textButton = screen.queryAllByTestId(textButtonTestid.textButtonId);
+    expect(cards.length).toBe(2);
+    expect(searchButton.length).toBe(1);
+    expect(avatar.length).toBe(1);
+    expect(typographies.length).toBe(4);
+    expect(textButton.length).toBe(1);
+  });
 });
--- lovely-github/src/pages/user-page/user-page.test.jsx
+++ lovely-github/src/pages/user-page/user-page.test.jsx
@@ -1,6 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 
+import { dataTestIds as cardDataTestid } from "../../components/card/card.component";
+import { dataTestIds as avatarDataTestid } from "../../components/avatar/avatar.component";
+import { dataTestIds as typographyDataTestid } from "../../components/typography/typography.component";
+import { dataTestIds as listDataTestid } from "../../components/list/list.component";
+import { dataTestIds as textButtonTestid } from "../../components/text-button/text-button.component";
 import UserPage from "./user-page.component";
 
 describe("Component <UserPage/>", () => {
@@ -19,8 +24,20 @@ describe("Component <UserPage/>", () => {
     const view = setUpComponent();
     expect(view).toBeDefined();
   });
-  //   it("Should correctly render screen", () => {
-  //     const view = setUpComponent();
-  //     expect(view).toBeDefined();
-  //   });
+
+  it("Should correctly render screen", () => {
+    setUpComponent();
+    const cards = screen.queryAllByTestId(cardDataTestid.card);
+    const avatar = screen.queryAllByTestId(avatarDataTestid.avatar);
+    const typographies = screen.queryAllByTestId(
+      typographyDataTestid.typography
+    );
+    const list = screen.queryAllByTestId(listDataTestid.list);
+    const textButton = screen.queryAllByTestId(textButtonTestid.textButtonId);
+    expect(cards.length).toBe(2);
+    expect(avatar.length).toBe(1);
+    expect(typographies.length).toBe(3);
+    expect(list.length).toBe(1);
+    expect(textButton.length).toBe(1);
+  });
 });
