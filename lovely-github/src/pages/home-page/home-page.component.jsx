c26d626cd18e329a14b2d495ea36405cdea2d2f1
MGianetti <mateusgianetti1996@gmail.com>
2022-02-04 00:35:13 -0300

    Implemented service use in HomePage

--- lovely-github/src/pages/home-page/home-page.component.jsx
+++ lovely-github/src/pages/home-page/home-page.component.jsx
@@ -10,21 +10,52 @@ import Card from "../../components/card";
 import Avatar from "../../components/avatar";
 import TextButton from "../../components/text-button";
 
+import userServices from "../../services/user";
+
 import { HOME_PAGE } from "./home-page.constants";
 
 import styles from "./home-page.module.scss";
 
 function HomePage(props) {
   const navigate = useNavigate();
+
+  const [userAvatar, setUserAvatar] = useState("/default_user.png");
+  const [userLogin, setUserLogin] = useState("");
   const [userName, setUserName] = useState("");
+  const [userBio, serUserBio] = useState("");
+
+  const [userNameSearch, setUserNameSearch] = useState("");
+  const [inputHasError, setInputHasError] = useState(false);
 
   const handleInputChange = (event) => {
-    setUserName(event.currentTarget.value);
+    setUserNameSearch(event.currentTarget.value);
+  };
+
+  const handleSearchButtonClick = async () => {
+    if (userNameSearch === "") {
+      setInputHasError(true);
+      return;
+    }
+    try {
+      const { avatar_url, login, name, bio } = await userServices.getDetails(
+        userNameSearch
+      );
+      setUserAvatar(avatar_url);
+      setUserLogin(login);
+      setUserName(name);
+      serUserBio(bio);
+      setInputHasError(false);
+      setUserNameSearch("");
+    } catch (error) {
+      // handle error
+    }
   };
 
   const handleSeeMoreClick = () => {
-    navigate("/user/mg");
+    navigate(`/user/${userLogin}`);
   };
+
+  const isSeeMoreDisabled = userName === "";
 
   return (
     <div>
@@ -33,24 +64,33 @@ function HomePage(props) {
           {HOME_PAGE.APP_TITLE}
         </Typography>
         <div className={styles.SearchForm}>
-          <Input value={userName} onChange={handleInputChange} />
-          <SearchButton className={styles.SearchButton} />
+          <Input
+            value={userNameSearch}
+            onChange={handleInputChange}
+            hasError={inputHasError}
+            errorMessage="Type an username"
+          />
+          <SearchButton
+            className={styles.SearchButton}
+            onClick={handleSearchButtonClick}
+          />
         </div>
       </Card>
       <Card className={styles.UserTeaserCard}>
-        <Avatar />
+        <Avatar src={userAvatar} />
         <Typography className={styles.UserName} type="h1">
-          Mateus Gianetti
+          {userName}
         </Typography>
         <Typography className={styles.Username} type="h2">
-          #MGianetti
+          {userLogin}
         </Typography>
         <Typography className={styles.UserBio} type="h3">
-          A focused React.js developer
+          {userBio}
         </Typography>
         <TextButton
           className={styles.SeeMoreButton}
           onClick={handleSeeMoreClick}
+          disabled={isSeeMoreDisabled}
         >
           {HOME_PAGE.TEXT_BUTTON_LABEL}
         </TextButton>
