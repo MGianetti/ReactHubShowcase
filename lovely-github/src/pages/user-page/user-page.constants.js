e8797e8cf203858a7b3571a1e9da98fb700e25c6
MGianetti <mateusgianetti1996@gmail.com>
2022-02-04 15:32:51 -0300

    Fixed minor ui and import files issues

--- lovely-github/src/pages/user-page/user-page.constants.js
+++ lovely-github/src/pages/user-page/user-page.constants.js
@@ -2,4 +2,6 @@ export const USER_PAGE = Object.freeze({
   REPOSITORIES: "Repos",
   USER_REPOS: "User repositories:",
   BACK_BUTTON: "Back",
+  EMPTY_REPOS: "Has no repositories yet",
+  EMPTY_DESCRIPTION: "This project does not have a description yet.",
 });
